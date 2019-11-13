7c5da3255ef5a018fe35f1356ec87d4f713aba7f
Danilo da Silva Pereira <dansilva41@hotmail.com>
2019-11-13 17:49:35 -0300

    Initial commit :: song-api

---
+++ src/test/java/br/com/silva/app/song/SongApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silva.app.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
